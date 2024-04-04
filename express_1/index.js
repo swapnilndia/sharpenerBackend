1d91e6a2c2ae50f7612b5c1dbf9701b4f4282bff
swapnilndia <swapnil240695@gmail.com>
2024-04-05 00:13:04 +0530

    express_2

--- express_1/index.js
+++ express_1/index.js
@@ -1,19 +1,26 @@
 const express = require('express');
-const bodyparser = require('body-parser')
-const cors = require('cors')
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-const app = express()
+const app = express();
 
-app.use(bodyparser.json())
-app.use(cors())
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(cors());
 
-// app.get('/', async(req, res) => {
-//     res.send('<h1>Hello to node js</h1>')
-// })
-app.get('/', async(req, res) => {
-    res.send({Body: 'hellow'})
-})
+app.get('/add-product', async (req, res) => {
+    res.send('<form action="/product" method="POST" ><input type="text" name="title" placeholder="add name of product"><input type="number" name="age" placeholder="add age of product"><button type="submit">Add Product</button></form>');
+});
+
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/', (req, res) => {
+    res.send('<h1>Hello to node js</h1>');
+});
 
 app.listen(3000, () => {
-    console.log('app is running at port 3000')
-})
+    console.log('app is running at port 3000');
+});
